3a8df4c89a6a0e58e8db65a297b09e294f39ece8
josephsdavid <josephsd@smu.edu>
2021-05-12 13:21:22 -0500

    working tokenizer??

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -11,14 +11,8 @@ sample_size=0.01
 path = "contigs/"
 
 files = [str(x) for x in Path(path).glob("**/*.txt")]
-
-
 tokenizer=Tokenizer(BPE())
-# range for vocab_size on partee's paper: 7500 -> 10000 (see page 10:
-# https://scholar.smu.edu/cgi/viewcontent.cgi?article=1150&context=datasciencereview)
 trainer = BpeTrainer(initial_alphabet=['a','g','c','t'])
-
 tokenizer.train(files[:4], trainer)
-
 tokenizer.save(path="json/BPE-4-contigs.json")
 import pdb; pdb.set_trace()
